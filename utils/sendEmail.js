124b6963c2d5984323323254dd288521959f4795
SVS Rohit <168812729+rohit02k5@users.noreply.github.com>
2025-12-14 13:22:07 +0530

    Change email transport to use secure port 465

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,8 +3,8 @@ const nodemailer = require("nodemailer");
 const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
-        port: 587,
-        secure: false, // true for 465, false for other ports
+        port: 465,
+        secure: true, // true for 465, false for other ports
         auth: {
             user: process.env.SMTP_EMAIL,
             pass: process.env.SMTP_PASSWORD,
